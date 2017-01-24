346af4b258dda291a5bcbd29489aafe251943223
orangekmt <kmt.team@orange.com>
2017-01-24 15:40:04 +0100

    fixed ephemeral template to use in memory storage

--- src/main/java/com/orange/todolist/TodosServlet.java
+++ src/main/java/com/orange/todolist/TodosServlet.java
@@ -15,15 +15,15 @@ public class TodosServlet extends HttpServlet {
 	
 	public TodoStorage getStorageService(){
 		//In Memory storage
-//		return new TodoStorageInMemory();
+		return new TodoStorageInMemory();
 		
 		//Use Mysql on Kermit
-		String login = System.getenv("MYSQL_USER");
-		String password = System.getenv("MYSQL_PASSWORD");
-		String host = System.getenv("MYSQL_SERVICE_HOST");
-		String port = System.getenv("MYSQL_SERVICE_PORT");
-		String database = System.getenv("MYSQL_DATABASE");
-		return new TodoStorageJDBCMysql(host, port, database, login, password);
+		//String login = System.getenv("MYSQL_USER");
+		//String password = System.getenv("MYSQL_PASSWORD");
+		//String host = System.getenv("MYSQL_SERVICE_HOST");
+		//String port = System.getenv("MYSQL_SERVICE_PORT");
+		//String database = System.getenv("MYSQL_DATABASE");
+		//return new TodoStorageJDBCMysql(host, port, database, login, password);
 	}
 
 	@Override
